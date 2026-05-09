5f3d3575348e308a7477b5972b82e834fd707918
Jacob Smit <jacobsmit@uvic.ca>
2026-05-09 11:25:26 -0700

    Initial commit with Astro, Tailwind, and Actions config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Required for GitHub Pages paths to work
+  site: 'https://jacobsmit.github.io',
+  
+  // Required for Tailwind 4
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
