4fad7d41db74b9e9b04c4ac21673d7a91802428a
Jacob Smit <jacobsmit@w134-87-143-181.wireless.uvic.ca>
2026-07-23 13:40:25 -0700

    optimize images, SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   // Your GitHub Pages full site URL (project site)
-  site: 'https://jacobsmit.github.io/liv-cafe-website', 
-  
+  site: 'https://jacobsmit.github.io/liv-cafe-website',
+
   // The exact name of your repository, starting with a forward slash
-  base: '/liv-cafe-website', 
-  
+  base: '/liv-cafe-website',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
